7a095fc113ef22c9c4be5f37d0c9d4117ad2fc0d
anuj kumar <an083339@gmail.com>
2021-07-29 21:41:28 +0600

    added files

---
+++ config/passport-google-oauth2-strategy.js
@@ -0,0 +1,44 @@
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const crypto = require('crypto');
+const User = require('../models/user');
+
+
+// tell passport to use a new strategy for google login
+passport.use(new googleStrategy({
+        clientID: '224033203476-vvrki3h9u8c044v6pfktb06dv1cppr5r.apps.googleusercontent.com',
+        clientSecret: 'X6ByX3XN7cKi81FamtgLaiI6',
+        callbackURL: "http://localhost:8000/users/auth/google/callback",
+    },
+
+    function(accessToken, refreshToken, profile, done){
+        // find a user
+        User.findOne({email: profile.emails[0].value}).exec(function(err, user){
+            if (err){console.log('error in google strategy-passport', err); return;}
+            console.log(accessToken, refreshToken);
+            console.log(profile);
+
+            if (user){
+                // if found, set this user as req.user
+                return done(null, user);
+            }else{
+                // if not found, create the user and set it as req.user
+                User.create({
+                    name: profile.displayName,
+                    email: profile.emails[0].value,
+                    password: crypto.randomBytes(20).toString('hex')
+                }, function(err, user){
+                    if (err){console.log('error in creating user google strategy-passport', err); return;}
+
+                    return done(null, user);
+                });
+            }
+
+        }); 
+    }
+
+
+));
+
+
+module.exports = passport;
---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const passportJWT = require('./config/passport-jwt-strategy');
+const passportGoogle = require('./config/passport-google-oauth2-strategy');
+
+const MongoStore = require('connect-mongo')(session);
+const sassMiddleware = require('node-sass-middleware');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
+
+
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+
+app.use(express.static('./assets'));
+// make the uploads path available to the browser
+app.use('/uploads', express.static(__dirname + '/uploads'));
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+
+
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// mongo store is used to store the session cookie in the db
+app.use(session({
+    name: 'codeial',
+    // TODO change the secret before deployment in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        
+        },
+        function(err){
+            console.log(err ||  'connect-mongodb setup ok');
+        }
+    )
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+app.use(flash());
+app.use(customMware.setFlash);
+
+// use express router
+app.use('/', require('./routes'));
+
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
