d562912c249a13f792965905f78105774b3ad4a2
anuj kumar <an083339@gmail.com>
2021-07-30 22:29:48 +0600

    Debugging

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.use('/', require('./routes'));
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+  port = 3000;
 }
 app.listen(port, function(err){
     if (err){
